f3516c448c9e16c8b1acbec2a24d8bae7b76c12d
TheoKusters <theo.kusters@advancedprograms.nl>
2022-09-30 16:30:47 +0200

    Setup IntelliJ structure with 1st tests - fix test

--- fps/src/test/scala/chapter02/Exercise2_1Suite.scala
+++ fps/src/test/scala/chapter02/Exercise2_1Suite.scala
@@ -5,10 +5,10 @@ import chapter02.FormatCalcFunctions.*
 
 class Exercise2_1Suite {
 
-  assert(formatResult("absolute value", -42, abs) == "The absolute value of -42 is 42", 
+  assert(formatResult("absolute value", -42, abs) == "The absolute value of -42 is 42",
     "Error in the abs function")
   assert(formatResult("factorial", 7, factorial) == "The factorial of 7 is 5040",
-    "Error in the factorial function)
+    "Error in the factorial function")
   assert(formatResult("Fibonacci value in position", 8, fib) == "The 8th Fibonacci number is 13",
     "Error in the fib function")
 }
