e8e0baf17b5ea4547d896814961438156e0c4bac
Theo Kusters <theo.kusters@advancedprograms.nl>
2022-10-03 11:11:35 +0200

    Setup IntelliJ structure with 1st tests (#11)
    * Setup IntelliJ structure with 1st tests
    
    * Setup IntelliJ structure with 1st tests - fix test
    
    * Marco's comments processed
    
    * Marco's comments processed 2
    
    * Marco's comments processed 3

---
+++ fps/src/main/scala/chapter02/Exercise2_1.scala
@@ -0,0 +1,53 @@
+package chapter02
+
+import annotation.*
+
+object MyCalcFunctions:
+
+    def abs(n: Int): Int =
+        if (n < 0) -n
+        else n
+
+    def factorial(n: Int): Int =
+        @tailrec
+        def go(n: Int, acc: Int): Int =
+            if (n <= 0) acc
+            else go(n - 1, acc * n)
+        go(n, 1)
+
+    //    Exercise 2.1
+    def fib(n: Int): Int =
+        @tailrec
+        def go(n: Int, fib1: Int, fib2: Int): Int =
+            if (n <= 1) fib1
+            else go(n - 1, fib2, fib1 + fib2)
+        go(n, 0, 1)
+
+object FormatCalcFunctions:
+
+    import MyCalcFunctions.*
+
+    def formatAbs(x: Int): String =
+        s"The absolute value of ${x} is ${abs(x)}"
+
+    def formatFactorial(x: Int): String =
+        s"The factorial of ${x} is ${factorial(x)}"
+
+    def formatFib(x: Int): String =
+        s"The ${x}th Fibonacci number is ${fib(x)}"
+
+    def formatResult(name: String, n: Int, f: Int => Int) =
+        s"The ${name} of ${n} is ${f(n)}"
+
+object PrintCalcFunctions extends App:
+
+    import MyCalcFunctions.*
+    import FormatCalcFunctions.*
+
+    println(formatAbs(-42))
+    println(formatFactorial(7))
+    println(formatFib(8))
+
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial))
+    println(formatResult("Fibonacci value in position", 8, fib))
