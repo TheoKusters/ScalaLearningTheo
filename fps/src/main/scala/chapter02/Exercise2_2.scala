7474263fe9053722b8c4497d07d0c826138bb0aa
TheoKusters <theo.kusters@advancedprograms.nl>
2022-10-03 10:28:51 +0200

    Marco's comments processed

--- fps/src/main/scala/chapter02/Exercise2_2.scala
+++ fps/src/main/scala/chapter02/Exercise2_2.scala
@@ -1,24 +1,26 @@
 package chapter02
 
-  object IsSorted:
+import annotation.*
 
-  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
-    @annotation.tailrec
-    def loop(n: Int): Boolean = {
+object IsSorted:
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean =
+    @tailrec
+    def loop(n: Int): Boolean =
       if n >= as.length - 1 then true
       else if !ordered(as(n), as(n + 1)) then false
       else loop(n + 1)
-    }
+
     loop(0)
-  }
 
-    object printIsSorted:
+object printIsSorted extends App:
+
     import IsSorted.*
-
-    @main def printIsSorted: Unit =
-      println(isSorted(Array(1,2,3,4,5), (a: Int, b: Int)=> b >= a))
-      println(isSorted(Array(-0), (a: Int, b: Int)=> b >= a))
-      println(!isSorted(Array(1,6,9,2,12), (a: Int, b: Int)=> b >= a))
-      println(isSorted(Array('A','B'), (a: Char, b: Char)=> b >= a))
-      println(isSorted(Array('a','B'), (a: Char, b: Char)=> b >= a))
-      println(isSorted(Array(true,false), (a: Boolean, b: Boolean)=> b >= a))
+//    def printIsSorted: Unit =
+    println(isSorted(Array(1,2,3,4,5), (a: Int, b: Int)=> b >= a))
+    println(isSorted(Array(-0), (a: Int, b: Int)=> b >= a))
+    println(!isSorted(Array(1,6,9,2,12), (a: Int, b: Int)=> b >= a))
+    println(isSorted(Array('A','B'), (a: Char, b: Char)=> b >= a))
+    println(isSorted(Array('a','B'), (a: Char, b: Char)=> b >= a))
+    println(isSorted(Array(true,false), (a: Boolean, b: Boolean)=> b >= a))
+    
